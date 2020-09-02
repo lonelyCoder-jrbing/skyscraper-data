0bf2730f655295f551aab0fb3ad68297be490476
lonelyCoder-jrbing <719203729@qq.com>
2020-09-02 21:33:44 +0800

    项目骨架

---
+++ skyscraper-data/src/main/java/com/skyscraper/skyscraperdata/SkyscraperDataApplication.java
@@ -0,0 +1,13 @@
+package com.skyscraper.skyscraperdata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkyscraperDataApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SkyscraperDataApplication.class, args);
+    }
+
+}
