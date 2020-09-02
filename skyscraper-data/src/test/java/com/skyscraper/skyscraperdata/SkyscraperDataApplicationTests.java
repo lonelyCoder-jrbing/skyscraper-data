5247fefa4523588025394b0574579069e59f4543
lonelyCoder-jrbing <719203729@qq.com>
2020-09-02 21:34:37 +0800

    项目骨架

---
+++ skyscraper-data/src/test/java/com/skyscraper/skyscraperdata/SkyscraperDataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skyscraper.skyscraperdata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkyscraperDataApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
